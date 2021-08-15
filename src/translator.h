106d95f75301405eafe09c4d9a0fc624a3232a3d
Alexander <krikun.contact@ya.ru>
2021-08-15 21:39:43 +0700

    Add comments

--- src/translator.h
+++ src/translator.h
@@ -3,21 +3,28 @@
 namespace dragon_book
 {
 
+///@brief Simple infix to postfix form translator
 class Parser final
 {
 public:
+    ///@brief Constructor
+    ///
+    /// Reads from stdin
     Parser();
 
     ~Parser() = default;
 
+    ///@brief Parse 'expr' and 'rest' nonterminals
     void Expr();
 
+    ///@brief Parse terminals
     void Term();
 
+    ///@brief Parse concrete terminal and read next symbol
     void Match(char terminal);
 
 private:
-    char lookahead_;
+    char lookahead_; ///< current symbol
 };
 
 } // namespace dragon_book
